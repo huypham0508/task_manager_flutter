eda97b5a4024a60f2990b6d45a96ec8e64038919
Mạnh Huy <phammanhhuy1107@gmail.com>
2025-03-21 13:06:00 +0700

    [built][detail page]

---
+++ lib/presentation/components/color_picker.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ColorPicker extends StatelessWidget {
+  final Color selectedColor;
+  final List<Color> colorOptions;
+  final Function(Color) onColorSelected;
+
+  const ColorPicker({
+    super.key,
+    required this.selectedColor,
+    required this.colorOptions,
+    required this.onColorSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 10,
+      children:
+          colorOptions.map((color) {
+            return InkWell(
+              onTap: () => onColorSelected(color),
+              borderRadius: BorderRadius.circular(20),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                width: selectedColor == color ? 45 : 40,
+                height: selectedColor == color ? 45 : 40,
+                decoration: BoxDecoration(
+                  color: color,
+                  shape: BoxShape.circle,
+                  boxShadow:
+                      selectedColor == color
+                          ? [
+                            BoxShadow(
+                              color: color.withOpacity(0.5),
+                              blurRadius: 10,
+                              spreadRadius: 3,
+                            ),
+                          ]
+                          : [],
+                  border: Border.all(
+                    color:
+                        selectedColor == color
+                            ? Colors.black
+                            : Colors.transparent,
+                    width: 3,
+                  ),
+                ),
+              ),
+            );
+          }).toList(),
+    );
+  }
+}
--- lib/presentation/screens/task_detail_screen.dart
+++ lib/presentation/screens/task_detail_screen.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:task_manager_app/core/utils/custom_snackbar.dart';
+import 'package:task_manager_app/core/utils/helper.dart';
+import 'package:task_manager_app/presentation/components/color_picker.dart';
+import 'package:task_manager_app/presentation/components/subtask_list.dart';
+
 import '../../data/models/task_model.dart';
 import '../providers/task_provider.dart';
 
 class TaskDetailScreen extends StatefulWidget {
   final Task? task;
-
   const TaskDetailScreen({super.key, this.task});
-
   @override
   TaskDetailScreenState createState() => TaskDetailScreenState();
 }
@@ -19,7 +20,18 @@ class TaskDetailScreenState extends State<TaskDetailScreen> {
   late TextEditingController _titleController;
   late TextEditingController _descriptionController;
   late DateTime _selectedDueDate;
+  late Color _selectedColor;
   bool _isCompleted = false;
+  List<SubTask> _subTasks = [];
+
+  final List<Color> _colorOptions = [
+    Colors.blue,
+    Colors.deepPurple,
+    Colors.red,
+    Colors.orange,
+    Colors.purple,
+    Colors.teal,
+  ];
 
   @override
   void initState() {
@@ -30,6 +42,8 @@ class TaskDetailScreenState extends State<TaskDetailScreen> {
     );
     _selectedDueDate = widget.task?.dueDate ?? DateTime.now();
     _isCompleted = widget.task?.isCompleted ?? false;
+    _subTasks = widget.task?.subTasks ?? [];
+    _selectedColor = widget.task?.color ?? Colors.blue;
   }
 
   @override
@@ -37,6 +51,24 @@ class TaskDetailScreenState extends State<TaskDetailScreen> {
     _titleController.dispose();
     _descriptionController.dispose();
     super.dispose();
+  }
+
+  void _selectColor(Color color) {
+    setState(() {
+      _selectedColor = color;
+    });
+  }
+
+  void _addSubTask() {
+    setState(() {
+      _subTasks.add(SubTask(title: "Công việc phụ mới", isCompleted: false));
+    });
+  }
+
+  void _removeSubTask(int index) {
+    setState(() {
+      _subTasks.removeAt(index);
+    });
   }
 
   Future<void> _selectDueDate(BuildContext context) async {
@@ -49,7 +81,6 @@ class TaskDetailScreenState extends State<TaskDetailScreen> {
 
     if (pickedDate != null) {
       TimeOfDay? pickedTime = await showTimePicker(
-        // ignore: use_build_context_synchronously
         context: context,
         initialTime: TimeOfDay.fromDateTime(_selectedDueDate),
       );
@@ -70,16 +101,37 @@ class TaskDetailScreenState extends State<TaskDetailScreen> {
 
   void _saveTask() {
     if (_formKey.currentState!.validate()) {
+      final taskProvider = Provider.of<TaskProvider>(context, listen: false);
+      final existingSubTasks = widget.task?.subTasks ?? [];
+
+      final deletedSubTasks =
+          existingSubTasks
+              .where(
+                (oldSubTask) =>
+                    !_subTasks.any(
+                      (newSubTask) => newSubTask.id == oldSubTask.id,
+                    ),
+              )
+              .toList();
+
+      for (var subTask in deletedSubTasks) {
+        taskProvider.deleteTask(subTask.id!);
+      }
+
       final task = Task(
         id: widget.task?.id,
         title: _titleController.text.trim(),
         description: _descriptionController.text.trim(),
-        isCompleted: _isCompleted,
+        isCompleted:
+            _subTasks.isEmpty
+                ? _isCompleted
+                : _subTasks.every((st) => st.isCompleted),
         dueDate: _selectedDueDate,
         createdAt: widget.task?.createdAt ?? DateTime.now(),
+        subTasks: _subTasks,
+        color: _selectedColor,
       );
 
-      final taskProvider = Provider.of<TaskProvider>(context, listen: false);
       if (widget.task == null) {
         taskProvider.addTask(task);
         CustomSnackbar.show(context, 'Công việc đã được thêm!');
@@ -94,6 +146,9 @@ class TaskDetailScreenState extends State<TaskDetailScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final isDarkMode = theme.brightness == Brightness.dark;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -117,56 +172,156 @@ class TaskDetailScreenState extends State<TaskDetailScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _titleController,
-                decoration: const InputDecoration(labelText: 'Tên công việc'),
-                validator:
-                    (value) =>
-                        value!.isEmpty ? 'Vui lòng nhập tên công việc' : null,
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: _descriptionController,
-                decoration: const InputDecoration(labelText: 'Mô tả công việc'),
-                maxLines: 3,
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      "Hạn hoàn thành: ${DateFormat('dd/MM/yyyy HH:mm').format(_selectedDueDate)}",
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    hintText: 'Write a title...',
+                    filled: true,
+                    fillColor: isDarkMode ? Colors.grey[800] : Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintStyle: TextStyle(
+                      color:
+                          isDarkMode
+                              ? Colors.white.withValues(alpha: 0.5)
+                              : Colors.black.withValues(alpha: 0.5),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
                     ),
                   ),
-                  const Spacer(),
-                  ElevatedButton(
-                    onPressed: () => _selectDueDate(context),
-                    child: const Text("Chọn ngày & giờ"),
+                  style: TextStyle(
+                    color: isDarkMode ? Colors.white : Colors.black,
                   ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  const Text("Hoàn thành: "),
-                  Checkbox(
-                    value: _isCompleted,
-                    onChanged: (value) {
-                      setState(() {
-                        _isCompleted = value!;
-                      });
-                    },
+                  maxLines: 1,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Title cannot be empty';
+                    }
+                    if (value.length < 3) {
+                      return 'Title must be at least 3 characters';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _descriptionController,
+                  style: TextStyle(
+                    color: isDarkMode ? Colors.white : Colors.black,
                   ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _saveTask,
-                child: const Text("Lưu công việc"),
-              ),
-            ],
+                  decoration: InputDecoration(
+                    hintText: "Write a note...",
+                    hintStyle: TextStyle(
+                      color:
+                          isDarkMode
+                              ? Colors.white.withValues(alpha: 0.5)
+                              : Colors.black.withValues(alpha: 0.5),
+                    ),
+                    filled: true,
+                    fillColor: isDarkMode ? Colors.grey[800] : Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  maxLines: 4,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Description cannot be empty';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Chọn màu nền:",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ColorPicker(
+                  selectedColor: _selectedColor,
+                  colorOptions: _colorOptions,
+                  onColorSelected: _selectColor,
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Due date",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => _selectDueDate(context),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: 8,
+                        ),
+                        decoration: BoxDecoration(
+                          color: isDarkMode ? Colors.grey[800] : Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              Helper.formatTime(_selectedDueDate),
+                              style: TextStyle(
+                                color: isDarkMode ? Colors.white : Colors.black,
+
+                                fontSize: 14,
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            const Icon(
+                              Icons.arrow_drop_down,
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                SubTaskListWidget(
+                  subTasks: _subTasks,
+                  onDeleteSubTask: _removeSubTask,
+                ),
+                const SizedBox(height: 8),
+                GestureDetector(
+                  onTap: _addSubTask,
+                  child: Row(
+                    children: const [
+                      Icon(Icons.add, color: Colors.grey),
+                      SizedBox(width: 8),
+                      Text(
+                        "Add a subtask",
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _saveTask,
+                  child: const Text("Lưu công việc"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
